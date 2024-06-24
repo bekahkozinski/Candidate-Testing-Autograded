7385099ef4d2ff3ee48e6efeacbe4a791e7c16e1
ezekiel chavez <bekahkozinski@gmail.com>
2024-06-24 17:10:10 -0500

    Candidate Testing Part 2 RKozinski

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,7 @@ let candidateAnswer = " "
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 *10 = ?", "Given the array [8, orbit, trajectory, 45] what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -35,20 +35,12 @@ function gradeQuiz(candidateAnswers) {
   // console.log("Correct!");
 // } else {
   // console.log("Incorrect, please try again");
+//  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+//  return grade;
+  for (let i = 0; i < questions.length; i++) {
+    console.log(`Question ${[i+1]}- Your answer: ${candidateAnswers[i]}, Correct answer: ${correctAnswers[i]}`)
+  }
 }
-
-let displayAnswers = `Question 1- Your answer: ${candidateAnswers[0]}, Correct answer: ${correctAnswers[0]}
-Question 2- Your answer: ${candidateAnswers[1]}, Correct answer: ${correctAnswers[1]}
-Question 3- Your answer: ${candidateAnswers[2]}, Correct answer: ${correctAnswers[2]}
-Question 4- Your answer: ${candidateAnswers[3]}, Correct answer: ${correctAnswers[3]}
-Question 5- Your answer: ${candidateAnswers[4]}, Correct answer: ${correctAnswers[4]}`
-
-console.log(displayAnswers);
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
 
   function runProgram() {
     askForName();
